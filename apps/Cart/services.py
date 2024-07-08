548aa85df4c0017c944a8e48b620f10ccc85fb0f
YoussifAllam <96921160+YoussifAllam@users.noreply.github.com>
2024-07-08 12:24:11 +0300

    upload remove , calc ,clear

--- apps/Cart/services.py
+++ apps/Cart/services.py
@@ -1,11 +1,12 @@
 from .models import Cart_Items
 from datetime import datetime
-from apps.Shipping.models import Shipping
+from apps.Shipping.models import Shipping , INSURANCE
 
 
-def get_Shipping_Flat_rate():
+def get_Shipping_and_INSURANCE_rate():
     Shipping_rate = Shipping.objects.get(id = 1)
-    return Shipping_rate.flatRate
+    INSURANCE_rate = INSURANCE.objects.get(id = 1)
+    return Shipping_rate.flatRate , INSURANCE_rate.INSURANCE
 
 def Check_if_Cart_Item_Exists(cart_id, dress_id):
     try:
@@ -14,32 +15,39 @@ def Check_if_Cart_Item_Exists(cart_id, dress_id):
     except Cart_Items.DoesNotExist:
         return False , None
     
+def get_dress_price( obj):
+        booking_days = int(obj.booking_for_n_days)
+        if booking_days == 3:
+            return obj.dress.price_for_3days
+        elif booking_days == 6:
+            return obj.dress.price_for_6days
+        elif booking_days == 8:
+            return obj.dress.price_for_8days
+        return None
+
 def calculate_total_price(cart_items ):
     total_price = 0
     Subtotal = 0
     for item in cart_items:
-        sale_price = item.product_size.Product_sale_price
-        regular_price = item.product_size.Product_regular_price
-        if sale_price != 0 :
-            Subtotal += sale_price * item.quantity
-        else : 
-            Subtotal += regular_price * item.quantity
+        price = get_dress_price(item)
+        Subtotal+=price
 
-        Shipping_Flat_rate = get_Shipping_Flat_rate()
-        total_price = Subtotal + Shipping_Flat_rate
-    return  Subtotal , total_price , Shipping_Flat_rate
+    Shipping_Flat_rate , INSURANCE_rate = get_Shipping_and_INSURANCE_rate()
+    total_price = Subtotal + Shipping_Flat_rate  + INSURANCE_rate
+    return  Subtotal , total_price , Shipping_Flat_rate , INSURANCE_rate
     
 def calc_total_price_with_coupon(Subtotal, coupon):
     valid_to = coupon.valid_to.date()  # Convert to date
     discount = coupon.discount
     today_date = datetime.today().date()
 
-    Shipping_Flat_rate = get_Shipping_Flat_rate()
+    Shipping_Flat_rate , INSURANCE_rate = get_Shipping_and_INSURANCE_rate()
 
 
     if valid_to >= today_date:
         Subtotal -= (Subtotal * (discount / 100))
-        total_price = Subtotal + Shipping_Flat_rate
+        total_price = Subtotal + Shipping_Flat_rate  + INSURANCE_rate
         return Subtotal,total_price ,  True
     else:
         return Subtotal, 0 , False
+    
--- apps/Cart/views.py
+++ apps/Cart/views.py
@@ -34,7 +34,7 @@ def add_item(request):
         is_cart_item_founded , cart_items = Check_if_Cart_Item_Exists(cart_id=Cart_id, dress_id=Dress_id)
         if is_cart_item_founded: return Response ({ 'status': 'fialed' , 'error':'The item is already in the cart'}, status=status.HTTP_400_BAD_REQUEST)
         else :  
-            if booking_for_n_days not in [3,6,8] : return Response({'status': 'fialed' , 'error': 'booking_for_n_days must be 3,6 or 8'}, status=status.HTTP_400_BAD_REQUEST)
+            if booking_for_n_days not in ['3','6','8'] : return Response({'status': 'fialed' , 'error': 'booking_for_n_days must be 3,6 or 8'}, status=status.HTTP_400_BAD_REQUEST)
             
             cart_items = Cart_Items.objects.create(cart=cart, dress=dress,  booking_start_date=booking_start_date,
                                                         booking_end_date=booking_end_date, booking_for_n_days=booking_for_n_days, )
@@ -59,7 +59,7 @@ def get_cart_details(request):
         except Cart.DoesNotExist:
             return Response({'status': 'fialed' , 'error': 'Cart not found'}, status=status.HTTP_404_NOT_FOUND)
 
-@api_view(['DELETE']) #Todo ________________________________
+@api_view(['DELETE']) 
 def remove_item_from_cart(request): 
     if request.method == 'DELETE':
         cart_item_id = request.data.get('cart_item_id')
@@ -72,7 +72,7 @@ def remove_item_from_cart(request):
         except Cart_Items.DoesNotExist:
             return Response({'status': 'fialed' , 'error': 'Cart item not found'}, status=status.HTTP_404_NOT_FOUND)
 
-@api_view(['DELETE']) #Todo ________________________________
+@api_view(['DELETE']) 
 def clear_shopping_cart(request): 
     if request.method == 'DELETE':
         cart_id = request.data.get('cart_id')
@@ -86,32 +86,29 @@ def clear_shopping_cart(request):
         except Cart.DoesNotExist:
             return Response({'status': 'fialed' , 'error': 'cart not found'}, status=status.HTTP_404_NOT_FOUND)
 
-@api_view(['GET'])    #Todo ________________________________
+@api_view(['GET'])    
 def cart_total_price(request):
     cart_id = request.data.get('cart_id')
     coupon_code = request.data.get('coupon_code')
-    try:
-        cart = Cart.objects.get(id=cart_id)
-    except Cart.DoesNotExist:
-        return Response({'status': 'fialed' , 'error': 'Cart not found'}, status=status.HTTP_404_NOT_FOUND)
+    try: cart = Cart.objects.get(id=cart_id)
+    except Cart.DoesNotExist:  return Response({'status': 'fialed' , 'error': 'Cart not found'}, status=status.HTTP_404_NOT_FOUND)
     
-    cart_items = cart.items.all()
-    Subtotal , total_price_without_coupon , Shipping_Flat_rate = calculate_total_price(cart_items )
+    cart_items = cart.items_set.all()
+    Subtotal , total_price_without_coupon , Shipping_Flat_rate , INSURANCE_rate = calculate_total_price(cart_items )
     
     if coupon_code:
-        try:
-            coupon = Coupon.objects.get(code=coupon_code)
-        except Coupon.DoesNotExist:
-            return Response({'status': 'fialed' , 'error': 'Coupon not found'}, status=status.HTTP_404_NOT_FOUND)
+        try: coupon = Coupon.objects.get(code=coupon_code)
+        except Coupon.DoesNotExist: return Response({'status': 'fialed' , 'error': 'Coupon not found'}, status=status.HTTP_404_NOT_FOUND)
         
         sale_price , total_price ,  is_coupon_valid = calc_total_price_with_coupon(Subtotal , coupon)
-        if not is_coupon_valid : 
-            return Response({ 'status': 'field', 'coupon' : 'the_coupon_is_expired' , }, status=status.HTTP_400_BAD_REQUEST)
-        else : 
-            return Response({ 'status': 'success','total_price_without_coupon': Subtotal , 'total_price_after_coupon' : sale_price ,
-                               'Shipping_Flat_rate' : Shipping_Flat_rate ,'total_price': total_price}, status=status.HTTP_200_OK)
+        if not is_coupon_valid : return Response({ 'status': 'field', 'coupon' : 'the_coupon_is_expired' , }, status=status.HTTP_400_BAD_REQUEST)
+        else : return Response({ 'status': 'success','total_price_without_coupon': Subtotal , 'total_price_after_coupon' : sale_price ,
+                               'Shipping_Flat_rate' : Shipping_Flat_rate ,'INSURANCE' : INSURANCE_rate,'total_price': total_price}, status=status.HTTP_200_OK)
 
-    return Response({ 'status': 'success', 'subtotal' : Subtotal  , 'Shipping_Flat_rate' : Shipping_Flat_rate,
+    return Response({ 'status': 'success', 
+                     'subtotal' : Subtotal  ,
+                     'DELIVERY' : Shipping_Flat_rate,
+                     'INSURANCE' : INSURANCE_rate,
                      'total_price': total_price_without_coupon}, status=status.HTTP_200_OK)
 
 
