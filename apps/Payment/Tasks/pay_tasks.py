659f74f825ba2f648967948c689c08ae1b56c47f
YoussifAllam <96921160+YoussifAllam@users.noreply.github.com>
2024-07-30 13:19:09 +0300

    upload stc and applepay normal  Payment

--- apps/Payment/Tasks/pay_tasks.py
+++ apps/Payment/Tasks/pay_tasks.py
@@ -1,4 +1,4 @@
-
+#test
 import base64
 import requests
 from rest_framework import status
@@ -9,41 +9,62 @@ import uuid
 logger = logging.getLogger(__name__)
 from ..models import Payment
 
+
 def create_moyasar_payment(request_data, validated_data):
-        api_key = settings.SECRET_KEY
-        encoded_api_key = base64.b64encode(api_key.encode()).decode()
-        headers = {
+    api_key = settings.SECRET_KEY
+    encoded_api_key = base64.b64encode(api_key.encode()).decode()
+    headers = {
             'Authorization': f'Basic {encoded_api_key}',
             'Content-Type': 'application/json',
         }
-        source = request_data['source']
-        payload = {
-            'amount': int(validated_data['amount'] *100),  # Amount in halalas so multiple it by  100 , 1 SAR = 100 halalas
-            'currency': 'SAR',
-            'description': validated_data['description'],
-            'source': {
-                'type': 'creditcard',
-                'name': source['name'],
-                'number': source['number'],
-                'cvc': source['cvc'],
-                'month': source['month'],
-                'year': source['year']
-            },
-            'callback_url': settings.CALLBACKURL
+        
+    source_type = request_data['source']['type']
+    
+    if source_type == 'creditcard':
+        source = {
+            'type': 'creditcard',
+            'name': request_data['source']['name'],
+            'number': request_data['source']['number'],
+            'cvc': request_data['source']['cvc'],
+            'month': request_data['source']['month'],
+            'year': request_data['source']['year']
         }
-        response = requests.post('https://api.moyasar.com/v1/payments', json=payload, headers=headers)
-        if response.status_code == 201:
-            payment_response = response.json()
-            return payment_response['id'], response
-        return None, response
-
+    elif source_type == 'stcpay':
+        source = {
+            'type': 'stcpay',
+            'mobile': request_data['source']['mobile']
+        }
+    elif source_type == 'applepay':
+        source = {
+            'type': 'applepay',
+            'token': request_data['source']['token']
+        }
+    else:
+        return None, Response({"error": "Unsupported payment type"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    payload = {
+        'amount': int(validated_data['amount']) * 100,  # Convert to halalas
+        'currency': 'SAR',
+        'description': validated_data['description'],
+        'source': source,
+        'callback_url': settings.CALLBACKURL
+    }
+    
+    response = requests.post('https://api.moyasar.com/v1/payments', json=payload, headers=headers)
+    
+    if response.status_code == 201:
+        payment_response = response.json()
+        return payment_response['id'], response
+    
+    return None, response
 
 def process_callback(request):
         payment_data = request.data if request.method == 'POST' else request.query_params
         logger.info(f"Callback received: {payment_data}")
 
         payment_id = payment_data.get('id')
-        payment_status = payment_data.get('status')
+        # payment_status = payment_data.get('status')
+        payment_status = 'pending'
 
         # Log the received payment_id and status
         logger.info(f"Received payment_id: {payment_id}, payment_status: {payment_status}")
@@ -64,3 +85,31 @@ def process_callback(request):
         except Payment.DoesNotExist:
             logger.error(f"Payment not found for ID: {payment_id}")
             return Response({"error": "Payment not found."}, status=status.HTTP_404_NOT_FOUND)
+        
+"""# def create_moyasar_payment(request_data, validated_data):
+#         api_key = settings.SECRET_KEY
+#         encoded_api_key = base64.b64encode(api_key.encode()).decode()
+#         headers = {
+#             'Authorization': f'Basic {encoded_api_key}',
+#             'Content-Type': 'application/json',
+#         }
+#         source = request_data['source']
+#         payload = {
+#             'amount': int(validated_data['amount'] *100),  # Amount in halalas so multiple it by  100 , 1 SAR = 100 halalas
+#             'currency': 'SAR',
+#             'description': validated_data['description'],
+#             'source': {
+#                 'type': 'creditcard',
+#                 'name': source['name'],
+#                 'number': source['number'],
+#                 'cvc': source['cvc'],
+#                 'month': source['month'],
+#                 'year': source['year']
+#             },
+#             'callback_url': settings.CALLBACKURL
+#         }
+#         response = requests.post('https://api.moyasar.com/v1/payments', json=payload, headers=headers)
+#         if response.status_code == 201:
+#             payment_response = response.json()
+#             return payment_response['id'], response
+#         return None, response"""
