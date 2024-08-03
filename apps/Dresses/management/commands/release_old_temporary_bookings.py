51711cb9820ca09d7074751420f5bb22ae75ee0a
YoussifAllam <96921160+YoussifAllam@users.noreply.github.com>
2024-08-03 19:06:05 +0300

    upload (delete not paid orders)

--- apps/Dresses/management/commands/release_old_temporary_bookings.py
+++ apps/Dresses/management/commands/release_old_temporary_bookings.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 from apps.Dresses.models import dress_busy_days
+from apps.orders.models import Order
 from datetime import timedelta
 
 class Command(BaseCommand):
@@ -11,4 +12,10 @@ class Command(BaseCommand):
         old_temporary_bookings = dress_busy_days.objects.filter(is_temporary=True, created_at__lt=expiration_time)
         count = old_temporary_bookings.count()
         old_temporary_bookings.delete()
-        self.stdout.write(self.style.SUCCESS(f'Released {count} old temporary bookings'))
+
+        orders_expiration_time = timezone.now() - timedelta(minutes=60)
+        not_paid_orders = Order.objects.filter(created_at__lt=orders_expiration_time , is_payment_completed =False)
+        orders_count = not_paid_orders.count()
+        not_paid_orders.delete()
+
+        self.stdout.write(self.style.SUCCESS(f'Released {count} old temporary bookings and {orders_count} not paid orders'))
