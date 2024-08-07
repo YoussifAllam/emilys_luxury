1178138aef1b89bd828ef816fd1ddae59c322a1e
YoussifAllam <96921160+YoussifAllam@users.noreply.github.com>
2024-08-07 21:25:25 +0300

    solve service file problem

--- apps/Dresses/services.py
+++ apps/Dresses/services.py
@@ -1,6 +1,6 @@
 from django.db.models import Func
-    from django.db.models import Avg  ,Q , Min ,Max
-    from rest_framework.pagination import PageNumberPagination
+from django.db.models import Avg  ,Q , Min ,Max
+from rest_framework.pagination import PageNumberPagination
 from .serializers import HomeDressesSerializer , AverageRatingSerializer
 from .models import dress_reviews  ,Dresses
 from collections import defaultdict
