9261c1163b492fa36d8229c95808e999cfb4798c
YoussifAllam <96921160+YoussifAllam@users.noreply.github.com>
2024-09-05 14:41:13 +0300

    add check of there is any item in the cart have error (The booking days are no longer available or The dress is not available for booking) in get cart details view

--- apps/Cart/serializers.py
+++ apps/Cart/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from apps.Dresses.models import Dresses, dress_images
 from .models import *
+from .Tasks import Cart_items_tasks
+
 
 class ProductImagesSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,10 +11,9 @@ class ProductImagesSerializer(serializers.ModelSerializer):
 
 class DressesSerializer(serializers.ModelSerializer):
     product_image = serializers.SerializerMethodField()
-
     class Meta:
         model = Dresses
-        fields = ['id', 'product_image', 'designer_name','actual_price', 'description','status']
+        fields = ['id', 'product_image', 'designer_name','actual_price', 'description','status' ]
 
     def get_product_image(self, obj):
         product_image = obj.image_set.first()
@@ -23,10 +24,18 @@ class DressesSerializer(serializers.ModelSerializer):
 class CartItemSerializer(serializers.ModelSerializer):
     dress = DressesSerializer()
     price = serializers.SerializerMethodField()
+    error = serializers.SerializerMethodField()
 
     class Meta:
         model = Cart_Items
-        fields = ['id', 'date_added', 'dress', 'booking_start_date', 'booking_end_date', 'booking_for_n_days', 'price']
+        fields = ['id', 'date_added', 'dress', 'booking_start_date', 'booking_end_date', 'booking_for_n_days', 'price', 'error']
+
+    def get_error(self, obj):
+        if not Cart_items_tasks.booking_days_is_available(obj.dress , obj.booking_start_date, obj.booking_end_date):
+            return 'The booking days are no longer available'
+        if not  Cart_items_tasks.dress_is_available(obj.dress):
+            return 'The dress is not available for booking'
+        return None
 
     def get_price(self, obj):
         booking_days = int(obj.booking_for_n_days)
