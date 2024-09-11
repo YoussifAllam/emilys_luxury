3b6208d20dd1d605da8f0f303cab32b8c6289555
YoussifAllam <96921160+YoussifAllam@users.noreply.github.com>
2024-09-11 15:32:57 +0300

    uncommint create busy days in pay view

--- apps/Payment/views.py
+++ apps/Payment/views.py
@@ -21,9 +21,9 @@ class CreatePaymentView(APIView):
             Target_order = selectors.get_order_by_uuid(order_uuid)
             if not Target_order: return Response({'status': 'failed','error': 'order not found'}, status=HTTP_400_BAD_REQUEST)
 
-            # Response_data , Response_status = order_tasks.create_busy_days_for_order(Target_order)
-            # if Response_status != HTTP_200_OK :
-            #     return Response(Response_data , Response_status)
+            Response_data , Response_status = order_tasks.create_busy_days_for_order(Target_order)
+            if Response_status != HTTP_200_OK :
+                return Response(Response_data , Response_status)
             
             payment_id,order_uuid , response = pay_tasks.create_moyasar_payment(request.data, serializer.validated_data , Target_order)
             if response.status_code == 201 and payment_id:
