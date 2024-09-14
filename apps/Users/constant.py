978cd0e96cb6a0d6fe43d4ef7cd4b5849d2d8967
YoussifAllam <96921160+YoussifAllam@users.noreply.github.com>
2024-09-14 15:42:45 +0300

    update current site value

--- apps/Users/constant.py
+++ apps/Users/constant.py
@@ -1,4 +1,4 @@
-current_site = 'emilys-luxury.com'
+current_site = 'emily.sa'
 # rest_password_url = 'https://emily-sa.vercel.app/auth/reset-password'
 rest_password_url = 'https://emily.sa/auth/reset-password'
 
