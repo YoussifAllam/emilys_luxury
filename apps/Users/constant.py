973cbf71df8b179609032a63aa3f716e8e832df2
YoussifAllam <96921160+YoussifAllam@users.noreply.github.com>
2024-09-14 11:09:56 +0300

    change the rest_password_url in constant user file

--- apps/Users/constant.py
+++ apps/Users/constant.py
@@ -1,2 +1,3 @@
 current_site = 'emilys-luxury.com'
-rest_password_url = 'https://emily-sa.vercel.app/auth/reset-password'
+# rest_password_url = 'https://emily-sa.vercel.app/auth/reset-password'
+rest_password_url = 'https://emily.sa/auth/reset-password'
