32adc1d0f7d1c3e2be44b959552f666fe96d778e
YoussifAllam <96921160+YoussifAllam@users.noreply.github.com>
2024-09-14 18:32:33 +0300

    update call backurl from vercel to live

--- apps/Payment/constant.py
+++ apps/Payment/constant.py
@@ -1,2 +1,3 @@
 from django.conf import settings
-CALL_BACK_URL='https://emily-sa.vercel.app/payment/callback'
+# CALL_BACK_URL='https://emily-sa.vercel.app/payment/callback'
+CALL_BACK_URL='https://emily.sa/payment/callback'
